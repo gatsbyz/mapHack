282d08009034dc121804e3a1abe0d335e17d0280
tjdals504 <sungminro92@gmail.com>
2017-05-20 14:54:21 -0400

    Authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, except: => [:index]
-end
+  before_action :authenticate_user!
+endg
