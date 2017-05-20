bb59971fe82a4a178365d9296d57c53994e942b3
tjdals504 <sungminro92@gmail.com>
2017-05-20 15:06:32 -0400

    Created models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520180030) do
+ActiveRecord::Schema.define(version: 20170520190418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cheats", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "start_point_lat"
+    t.integer  "start_point_long"
+    t.integer  "end_point_lat"
+    t.integer  "end_point_long"
+    t.integer  "user_id"
+    t.integer  "raiting"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "cheat_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +48,8 @@ ActiveRecord::Schema.define(version: 20170520180030) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
---
+++ test/models/cheat_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CheatTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
