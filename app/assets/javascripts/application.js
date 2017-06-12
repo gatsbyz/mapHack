fdd311b86847927d9a1a398790099053f0b54f55
gatsbyz <gatsby1118@gmail.com>
2017-06-12 00:44:07 -0400

    trying out requiring angular-ui-templates in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require angular
+//= require angular/angular
+//= require angular-rails-templates
 //= require angular-ui-router/release/angular-ui-router
 
 //= require bootstrap
